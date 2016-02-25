38bee19cd746a8d7226f5fa66bca2cda2b287a17
JesseAMoore90 <jesseamoore90@gamil.com>
2016-02-25 16:19:45 -0500

    Added User model and scaffolded

---
+++ FinancialWebApplication/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinancialWebApplication.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public bool LoggedIn { get; set; }
+        public DateTime CreatedDTTM { get; set; }
+        public DateTime UpdatedDTTM { get; set; }
+    }
+}
